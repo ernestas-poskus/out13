cd4cc0538a727d67890c1dc3ad2d123cf763ae40
Ernestas Poskus <ernestas.poskus@gmail.com>
2016-01-28 20:17:04 +0200

    Replace server.sh with Procfile

---
+++ Procfile
@@ -0,0 +1,2 @@
+hugo: hugo server --port=1313 --watch
+assets: cd static && grunt watch
--- server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-ip=$(ip addr show | grep inet  | tail -n 1 | cut -d ' ' -f 8)
-hugo server --bind=$ip --port=1313 --baseUrl=$ip --watch
